ad8f4591e626b616e3039cd1488707b8da0dce61
Nikita Saltykov <saltykovnikson@gmail.com>
2021-02-06 13:03:56 +0300

    create database

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
 from webapp.weather import get_weather
 from webapp.vc_ru_news import get_python_news
+from webapp.model import db
+
+
+
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
+    db.init_app(app)
 
     @app.route('/')
     def index():
@@ -13,4 +18,3 @@ def create_app():
         return render_template('index.html', weather=weather, news_list=news_list)
 
     return app
-
